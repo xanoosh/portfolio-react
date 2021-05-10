fe42eb5d351131a3acdf39f16af74850a3563995
xanoosh <xanoosh@gmail.com>
2021-05-11 01:42:57 +0200

    aos delay implementation, styling nav, components

--- src/components/main/ProjectCards.js
+++ src/components/main/ProjectCards.js
@@ -1,20 +1,17 @@
 import SingleProjectCard from './SingleProjectCard';
 
 const ProjectCards = ({ projectList }) => {
-  let AOSanimationDelay = 0;
-  const projects = projectList.map((project) => {
-    AOSanimationDelay += 100;
-    return (
-      <SingleProjectCard
-        key={project.id}
-        title={project.title}
-        path={project.path}
-        tags={project.tags}
-        description={project.description}
-        delayValue={AOSanimationDelay}
-      />
-    );
-  });
+  // let AOSanimationDelay = 0;
+  const projects = projectList.map((project, index) => (
+    <SingleProjectCard
+      key={project.id}
+      title={project.title}
+      path={project.path}
+      tags={project.tags}
+      description={project.description}
+      delayValue={index * 100}
+    />
+  ));
   return (
     <section className="project-list">
       <div className="row">{projects}</div>
