aa990482e07d7ae89146d21058c7f66c024ef7b8
xanoosh <xanoosh@gmail.com>
2021-05-11 01:54:42 +0200

    divided data from components, nav styling

---
+++ src/components/main/Data.js
@@ -0,0 +1,68 @@
+//list of projects:
+const projectsData = [
+  {
+    path: '/quiz',
+    title: 'Quiz',
+    id: 0,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'Quiz app with customizable categories, difficulty and number of questions. Fetches data from external API. ',
+  },
+  {
+    path: '/poker-calculator',
+    title: 'Texas Holdem Calculator',
+    id: 1,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'Calculates the odds of winning for a player. Basically plays 10 000 poker games then dispays result. ',
+  },
+  {
+    path: '/recipe-app',
+    title: 'Recipe app',
+    id: 2,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'This app allowes you to search for recipes by ingredients you have. Created with zero-waste approach in mind.',
+  },
+  {
+    path: '/dice-app',
+    title: 'Dice',
+    id: 3,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'Quiz app with customizable categories, difficulty and number of questions. Fetches data from external API. ',
+  },
+  {
+    path: '/memory-app',
+    title: 'Memory',
+    id: 4,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description: 'Just a memory game with different colors.',
+  },
+  {
+    path: '/snake',
+    title: 'Snake',
+    id: 5,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'DESKTOP ONLY Oldschool snake game, use arrow keys to move. Snake gets faster progressively. ',
+  },
+  {
+    path: '/timezone-app',
+    title: 'Timezone app',
+    id: 6,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'Simple app that calculates time in different time zones on input change. Consumes data from API on load.',
+  },
+  {
+    path: '/map-generator',
+    title: 'Map Generator',
+    id: 7,
+    tags: ['REST API', 'React', 'ES6', 'html', 'css'],
+    description:
+      'Randomly creates map for a board game. Resources on the map have to be in specific range.',
+  },
+];
+
+export default projectsData;
