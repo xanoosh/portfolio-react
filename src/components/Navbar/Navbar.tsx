1d611e75f94430292a96e32f5356f4751f63fd40
xanoosh <xanoosh@gmail.com>
2024-03-27 09:14:10 +0100

    vercel.json added, storybook build link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -55,6 +55,19 @@ export default function Navbar({
                 alt="storybook svg"
               />
             </a>
+            <a
+              href="/storybook-static/index.html"
+              target="_blank"
+              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <img
+                className="h-6 w-6"
+                src={storybookSvgPath}
+                alt="storybook svg"
+              />
+            </a>
           </div>
         </div>
       </div>
