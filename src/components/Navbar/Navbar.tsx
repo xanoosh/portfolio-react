55e9b3e6b56b18e22517b8501601ad6e13a3fc5f
xanoosh <xanoosh@gmail.com>
2024-03-30 12:30:57 +0100

    storybook link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar({
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <a
-              href="https://portfolio-react-vite-storybook.vercel.app"
+              href="https://portfolio-react-vite-storybook.vercel.app/storybook/index.html"
               target="_blank"
               className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
             >
