7f568e6d583f5cb4a746815850d8805f1f56ad1c
xanoosh <xanoosh@gmail.com>
2024-04-05 20:49:27 +0200

    projectcard ternary

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -17,18 +17,28 @@ export default function ProjectCard({
       transition={{ duration: 0.2 }}
       className="flex flex-col gap-4 backdrop-blur-sm max-w-sm p-6 border rounded-lg shadow bg-slate-800/50 border-slate-700"
     >
-      <h3 className="text-2xl font-bold tracking-tight text-white">{title}</h3>
-      <p className="font-normal text-slate-300">{description}</p>
-      <div className="flex flex-row gap-2 flex-wrap">
-        {badges.map((badge, i: number) => (
-          <Badge key={i} text={badge} />
-        ))}
-      </div>
-      <div className="flex flex-row sm:flex-nowrap flex-wrap mt-auto gap-4">
-        {buttons.map(({ text, url, icon }, i: number) => (
-          <Button key={i} text={text} url={url} icon={icon} />
-        ))}
-      </div>
+      {title ? (
+        <h3 className="text-2xl font-bold tracking-tight text-white">
+          {title}
+        </h3>
+      ) : null}
+      {description ? (
+        <p className="font-normal text-slate-300">{description}</p>
+      ) : null}
+      {badges ? (
+        <div className="flex flex-row gap-2 flex-wrap">
+          {badges.map((badge, i: number) => (
+            <Badge key={i} text={badge} />
+          ))}
+        </div>
+      ) : null}
+      {buttons ? (
+        <div className="flex flex-row sm:flex-nowrap flex-wrap mt-auto gap-4">
+          {buttons.map(({ text, url, icon }, i: number) => (
+            <Button key={i} text={text} url={url} icon={icon} />
+          ))}
+        </div>
+      ) : null}
     </motion.div>
   );
 }
