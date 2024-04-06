dcaa35ac7ef5e3837eac943f10c3c014bf09386f
xanoosh <xanoosh@gmail.com>
2024-04-06 17:55:04 +0200

    vitest setup

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Badge from './Badge';
+
+describe('Badge tests', () => {
+  it('Should have text equal to the badge text prop', () => {
+    render(<Badge text="badge" />);
+    expect(screen.getByText('badge')).toBeInTheDocument();
+  });
+
+  it('Should have classes bg-slate-400 text-slate-700 if variant prop was not passed', () => {
+    const { container } = render(<Badge text="badge" />);
+    expect(container.firstChild).toHaveClass('bg-slate-400 text-slate-700');
+  });
+
+  it('Should have classes bg-slate-400 text-slate-700 if variant prop is equal to main', () => {
+    const { container } = render(<Badge text="badge" variant="main" />);
+    expect(container.firstChild).toHaveClass('bg-slate-400 text-slate-700');
+  });
+});
