de82602dacb38f51c0b26c2e8466b12cc2e4a91b
xanoosh <xanoosh@gmail.com>
2024-04-15 13:37:55 +0200

    experience section prop ternary fix

--- src/components/ExperienceSection/ExperienceSection.tsx
+++ src/components/ExperienceSection/ExperienceSection.tsx
@@ -7,11 +7,11 @@ export default function ExperienceSection({
 }: ExperienceSectionProps) {
   return (
     <div className="flex flex-col gap-4 text-white">
-      {title.length > 0 ? (
+      {title && title.length > 0 ? (
         <h1 className="text-2xl font-semibold">{title}</h1>
       ) : null}
-      {text.length > 0 ? <p>{text}</p> : null}
-      {list.length > 0 ? (
+      {text && text.length > 0 ? <p>{text}</p> : null}
+      {list && list.length > 0 ? (
         <ol reversed className="list-decimal pl-8">
           {list.map(({ title, date, responsibilities }, i) => (
             <li key={i} className="marker:text-3xl mb-4">
