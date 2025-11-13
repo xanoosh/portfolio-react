54a6fffa2262e84cd4baff594fdf75ebed014c8b
xanoosh <xanoosh@gmail.com>
2025-11-13 09:50:34 +0100

    tooltip story update

--- src/components/TooltipComponent/TooltipComponent.stories.tsx
+++ src/components/TooltipComponent/TooltipComponent.stories.tsx
@@ -1,5 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import TooltipComponent from './TooltipComponent';
+import { expect, userEvent } from 'storybook/test';
+import { TooltipProps } from '@radix-ui/react-tooltip';
+
+const tooltipTestAssertions = async (
+  tooltipTrigger: HTMLElement,
+  args: TooltipProps
+) => {
+  if (args?.defaultOpen) {
+    expect(tooltipTrigger).toHaveAttribute('data-state', 'instant-open');
+    await new Promise((resolve) => setTimeout(resolve, args.delayDuration));
+    await userEvent.unhover(tooltipTrigger);
+    await new Promise((resolve) => setTimeout(resolve, args.delayDuration));
+  }
+  expect(tooltipTrigger).toBeInTheDocument();
+  expect(tooltipTrigger).toHaveAttribute('data-state', 'closed');
+  await userEvent.hover(tooltipTrigger);
+  await new Promise((resolve) => setTimeout(resolve, args.delayDuration));
+  expect(tooltipTrigger).toHaveAttribute('data-state', 'delayed-open');
+  await userEvent.unhover(tooltipTrigger);
+  await new Promise((resolve) => setTimeout(resolve, args.delayDuration));
+  expect(tooltipTrigger).toHaveAttribute('data-state', 'closed');
+};
 
 const meta = {
   title: 'Portfolio/TooltipComponent',
@@ -14,8 +36,19 @@ export const BasicTooltipComponent: Story = {
   args: {
     text: 'tooltip text',
     defaultOpen: false,
-    delayDuration: 500,
-    children: <p>hover to see tooltip text</p>,
+    delayDuration: 300,
+    children: (
+      <p
+        data-testid="tooltip-trigger"
+        className="text-white underline cursor-pointer"
+      >
+        hover to see tooltip text
+      </p>
+    ),
+  },
+  play: async ({ canvas, args }) => {
+    const tooltipTrigger = canvas.getByTestId('tooltip-trigger');
+    tooltipTestAssertions(tooltipTrigger, args);
   },
 };
 
@@ -24,14 +57,36 @@ export const InstantOpenTooltipComponent: Story = {
     text: 'tooltip text',
     defaultOpen: false,
     delayDuration: 0,
-    children: <p>hover to see tooltip text instantly</p>,
+    children: (
+      <p
+        data-testid="tooltip-trigger"
+        className="text-white underline cursor-pointer"
+      >
+        hover to see tooltip text instantly
+      </p>
+    ),
+  },
+  play: async ({ canvas, args }) => {
+    const tooltipTrigger = canvas.getByTestId('tooltip-trigger');
+    tooltipTestAssertions(tooltipTrigger, args);
   },
 };
 export const DefaultOpenTooltipComponent: Story = {
   args: {
     text: 'tooltip text',
     defaultOpen: true,
-    delayDuration: 500,
-    children: <p>tooltip open on mount, hover to toggle.</p>,
+    delayDuration: 300,
+    children: (
+      <p
+        data-testid="tooltip-trigger"
+        className="text-white underline cursor-pointer"
+      >
+        tooltip open on mount, hover to toggle.
+      </p>
+    ),
+  },
+  play: async ({ canvas, args }) => {
+    const tooltipTrigger = canvas.getByTestId('tooltip-trigger');
+    tooltipTestAssertions(tooltipTrigger, args);
   },
 };
