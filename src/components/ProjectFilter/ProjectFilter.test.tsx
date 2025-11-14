7a7463f8ea95f2d68f45b87bdbfc89d41ac6970c
xanoosh <xanoosh@gmail.com>
2025-11-14 12:24:58 +0100

    fixed ProjectFilter test cases after interface update

--- src/components/ProjectFilter/ProjectFilter.test.tsx
+++ src/components/ProjectFilter/ProjectFilter.test.tsx
@@ -3,14 +3,15 @@ import { vi } from 'vitest';
 import ProjectFilter from './ProjectFilter';
 import userEvent from '@testing-library/user-event';
 
-  const setActiveBadgesMock = vi.fn();
-  const handleBadgeClickMock = vi.fn();
+const setActiveBadgesMock = vi.fn();
+const handleBadgeClickMock = vi.fn();
+const badges = ['badge1', 'badge2', 'badge3'];
 
 describe('ProjectFilter:', () => {
-
   it('Should display text "No active tag selected." if activeBadges array is empty', () => {
     render(
       <ProjectFilter
+        badges={badges}
         activeBadges={[]}
         setActiveBadges={() => {}}
         handleBadgeClick={() => {}}
@@ -22,6 +23,7 @@ describe('ProjectFilter:', () => {
   it('Should display badges if activeBadges array is not empty', () => {
     render(
       <ProjectFilter
+        badges={badges}
         activeBadges={['badge1', 'badge2']}
         setActiveBadges={() => {}}
         handleBadgeClick={() => {}}
@@ -34,6 +36,7 @@ describe('ProjectFilter:', () => {
   it('Should display button with id listbox-button', () => {
     const { container } = render(
       <ProjectFilter
+        badges={badges}
         activeBadges={[]}
         setActiveBadges={() => {}}
         handleBadgeClick={() => {}}
@@ -47,6 +50,7 @@ describe('ProjectFilter:', () => {
   it('handleBadgeClickMock function should be called on each badge button click', () => {
     const { container } = render(
       <ProjectFilter
+        badges={badges}
         activeBadges={['badge1', 'badge2']}
         setActiveBadges={() => {}}
         handleBadgeClick={handleBadgeClickMock}
@@ -59,23 +63,25 @@ describe('ProjectFilter:', () => {
     expect(handleBadgeClickMock).toHaveBeenCalledTimes(1);
   });
 
-  it('Should display div with role listbox when listbox-trigger button is clicked',async () => {
+  it('Should display div with role listbox when listbox-trigger button is clicked', async () => {
     const { container } = render(
       <ProjectFilter
+        badges={badges}
         activeBadges={[]}
         setActiveBadges={() => {}}
         handleBadgeClick={() => {}}
       />
     );
     userEvent.click(container.querySelector('button#listbox-button')!);
-     await waitFor(() =>
-     expect(container.querySelector('div[role=listbox]')).toBeInTheDocument()
+    await waitFor(() =>
+      expect(container.querySelector('div[role=listbox]')).toBeInTheDocument()
     );
   });
 
   it('setActiveBadges function should be called on each listbox element click', () => {
     const { container } = render(
       <ProjectFilter
+        badges={badges}
         activeBadges={[]}
         setActiveBadges={setActiveBadgesMock}
         handleBadgeClick={() => {}}
